47d655ddd101f72bfcc2ac984fb6beafd3d56ca6
Otgonbaatar <otgonbaatar.ganbold3@gmail.com>
2020-12-26 17:11:21 +0800

    created Links component -NiceCommit Otgnbtru

---
+++ src/components/Links/Links.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Links() {
+    return(
+        <div>
+            <div className="flex justify-center">
+                <h1 className="text-2xl font-bold text-gray-600">Чухал холбоос</h1>
+            </div>
+            <div className="w-full flex justify-center">
+                <div className="m-4 w-full grid grid-cols-1 gap-2 md:w-5/6 md:grid-cols-2 md:gap-3">
+                    <button className="bg-blue-500 w-full h-16 rounded-2xl font-sans text-white">Гарчиг</button>
+                    <button className="bg-blue-500 w-full h-16 rounded-2xl font-sans text-white">Гарчиг</button>
+                    <button className="bg-blue-500 w-full h-16 rounded-2xl font-sans text-white">Гарчиг</button>
+                    <button className="bg-blue-500 w-full h-16 rounded-2xl font-sans text-white">Гарчиг</button>
+                    <button className="bg-blue-500 w-full h-16 rounded-2xl font-sans text-white">Гарчиг</button>
+                    <button className="bg-blue-500 w-full h-16 rounded-2xl font-sans text-white">Гарчиг</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Slider from './components/slider';
 import Navbar from "./components/Navbar/Navbar";
+import Links from "./components/Links/Links";
 
 function Main() {
   
   return (
     <div>
       <Navbar />
+      <Links />
       <Slider />
     </div>
   )
