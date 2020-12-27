35f09079ee18020ff547d94f3947ce00e5f8ec6e
Otgonbaatar <otgonbaatar.ganbold3@gmail.com>
2020-12-27 12:46:32 +0800

    added edProgram v1

---
+++ src/components/EdProgram/EdProgram.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function EdProgram() {
+    return(
+        <div>
+            <div className="flex justify-center">
+                <h1 className="text-2xl font-bold text-gray-600">Сургалтын хөтөлбөр</h1>
+            </div>
+            <div className="flex justify-evenly">
+                <div className=" bg-blue-500 rounded-2xl w-1/5 my-4 text-white text-center">
+                    <h1 className="my-4">Test</h1>
+                    <p className="m-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt molestias ut architecto tempore ducimus doloribus, saepe quibusdam asperiores ad nulla?</p>
+                    <button className="bg-white my-4 text-black w-1/2 rounded-xl">More</button>
+                </div>
+                <div className=" bg-blue-500 rounded-2xl w-1/5 my-4 text-white text-center">
+                    <h1 className="my-4">Test</h1>
+                    <p className="m-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt molestias ut architecto tempore ducimus doloribus, saepe quibusdam asperiores ad nulla?</p>
+                    <button className="bg-white my-4 text-black w-1/2 rounded-xl">More</button>
+                </div>
+                <div className=" bg-blue-500 rounded-2xl w-1/5 my-4 text-white text-center">
+                    <h1 className="my-4">Test</h1>
+                    <p className="m-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt molestias ut architecto tempore ducimus doloribus, saepe quibusdam asperiores ad nulla?</p>
+                    <button className="bg-white my-4 text-black w-1/2 rounded-xl">More</button>
+                </div>
+                <div className=" bg-blue-500 rounded-2xl w-1/5 my-4 text-white text-center">
+                    <h1 className="my-4">Test</h1>
+                    <p className="m-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt molestias ut architecto tempore ducimus doloribus, saepe quibusdam asperiores ad nulla?</p>
+                    <button className="bg-white my-4 text-black w-1/2 rounded-xl">More</button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Slider from './components/slider';
 import Navbar from "./components/Navbar/Navbar";
 import Links from "./components/Links/Links";
+import EdProgram from "./components/EdProgram/EdProgram";
 
 function Main() {
   
   return (
     <div>
       <Navbar />
+      <EdProgram />
       <Links />
       <Slider />
     </div>
